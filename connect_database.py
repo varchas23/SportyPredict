a84a74d11735c19bbeb6a41d8be734e4ca7ce5c0
varchas23 <varchassharma23@gmail.com>
2024-05-18 02:27:43 -0400

    Connected to Neurelo DB instead of directly connecting to MongoDB

--- connect_database.py
+++ connect_database.py
@@ -1,8 +1,22 @@
-from pymongo import MongoClient
+# neurelo.py
+import os
+from neurelo.configuration import Configuration
+from neurelo.api_client import ApiClient
 
-# Replace with your MongoDB URI
-mongo_uri = "mongodb+srv://SportyPredictAdmin:Password@sportypredict.fpxpjl1.mongodb.net/"
-client = MongoClient(mongo_uri)
+class Neurelo:
+    def __init__(self):
+        # Load your Neurelo API key from environment variables
+        self.headers = {
+            "X-API-KEY": os.getenv("neurelo_9wKFBp874Z5xFw6ZCfvhXZTSUugkd3M4ibA7xlegbhIGhZI4yOwjmMc5wEartOOQDV0xktM63qVE9Bjyx8vPS7Q3Ye4LopNsjcuAJQ5FsTdk89EYdjcyBVyFGvKp5k+PBUGkoBu5G5ccVq3WyRTdmJqOsMX96kuM4p0jZNmxHazDiAVnDCkSLsn4Kel7aPpw_BJv0xnXZgPq0kYROUtIc0lLgL/7Vw/hx52bT0G0pRR4="),
+            "Content-Type": "application/json",
+        }
+        # Set the base URL for your Neurelo API
+        self.api_url = "https://us-east-2.aws.neurelo.com"
 
-# Test the connection
-print("Connected to MongoDB server successfully.")
+    def store(self):
+        # Implement your logic to store data in the Neurelo database
+        pass
+
+# Instantiate the Neurelo class and use its methods
+neurelo_instance = Neurelo()
+neurelo_instance.store()
